5158874cc114ac46aeb5d8d36de455d0494219b2
Andres Jaramillo <a.jaramillo24@gmail.com>
2021-09-30 03:21:21 +0000

    Removed hard coded values and replaced it wih object variables

--- src/js/index.js
+++ src/js/index.js
@@ -29,18 +29,36 @@ function render(variables = {}) {
   let cover = `<div class="cover"><img src="${variables.background}" /></div>`;
   if (variables.includeCover == false) cover = "<div class='cover'></div>";
 
+  let name = `<h1>${variables.name} ${variables.lastname}</h1>`;
+  if (variables.name == null && variables.lastname == null) name = `<h1></h1>`;
+  else if (variables.name != null && variables.lastname == null)
+    name = `<h1>${variables.name}</h1>`;
+  else if (variables.name == null && variables.lastname != null)
+    name = `<h1>${variables.lastname}</h1>`;
+
+  let role = `<h2>${variables.role}</h2>`;
+  if (variables.role == null) role = `<h2></h2>`;
+
+  let location = `<h3>${variables.city}, ${variables.country}</h3>`;
+  if (variables.city == null && variables.country == null)
+    location = `<h3></h3>`;
+  else if (variables.city != null && variables.country == null)
+    location = `<h3>${variables.city}</h3>`;
+  else if (variables.city == null && variables.country != null)
+    location = `<h3>${variables.country}</h3>`;
+
   // reset the website body with the new html output
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
-            ${cover}
+          ${cover}
           <img src="${variables.avatarURL}" class="photo" />
-          <h1>Lucy Boilett</h1>
-          <h2>Web Developer</h2>
-          <h3>Miami, USA</h3>
-          <ul class="position-right">
-            <li><a href="https://twitter.com/alesanchezr"><i class="fa fa-twitter"></i></a></li>
-            <li><a href="https://github.com/alesanchezr"><i class="fa fa-github"></i></a></li>
-            <li><a href="https://linkedin.com/alesanchezr"><i class="fa fa-linkedin"></i></a></li>
-            <li><a href="https://instagram.com/alesanchezr"><i class="fa fa-instagram"></i></a></li>
+          ${name}
+          ${role}
+          ${location}
+          <ul class="${variables.socialMediaPosition}">
+            <li><a href="${variables.twitter}"><i class="fa fa-twitter"></i></a></li>
+            <li><a href="${variables.github}"><i class="fa fa-github"></i></a></li>
+            <li><a href="${variables.linkedin}"><i class="fa fa-linkedin"></i></a></li>
+            <li><a href="${variables.instagram}"><i class="fa fa-instagram"></i></a></li>
           </ul>
         </div>
     `;
